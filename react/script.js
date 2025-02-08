77a644426e22d3fa7b50bc4eda3703a733bf1abd
7luas <159937802+7luas@users.noreply.github.com>
2025-02-08 12:05:17 +0000

    d

--- react/script.js
+++ react/script.js
@@ -1,1 +1,11 @@
-document.getElementById('app-container').innerHTML = "Hello World";
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Hello = React.createClass({
+ render: function () {
+ return <div>Hello {this.props.name}</div>;
+ }
+});
+ReactDOM.render(
+ <Hello name="World" />,
+ document.getElementById('react-container')
+);
