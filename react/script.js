23cf101b46eae743d2269aa3a14ec1271925dae1
7luas <159937802+7luas@users.noreply.github.com>
2025-02-09 13:22:27 +0000

    e

--- react/script.js
+++ react/script.js
@@ -1,7 +1,5 @@
-// app.js
-
-// Create a React element
-const element = React.createElement('h1', null, 'Hello, world from app.js!');
+// Create the React element
+const element = React.createElement('h1', null, 'Hello world...');
 
 // Render the React element to the DOM
 ReactDOM.render(element, document.getElementById('root'));
