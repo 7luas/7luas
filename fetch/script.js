12eddc9ab22355d8f18bec39f13e6c700f3cbee8
7luas <159937802+7luas@users.noreply.github.com>
2025-02-09 14:33:11 +0000

    e

--- fetch/script.js
+++ fetch/script.js
@@ -1,5 +1,22 @@
 alert("G");
 
+fetch('data.json')
+  .then(response => {
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON data
+  })
+  .then(data => {
+    // Use the loaded JSON data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
 if(0){
 getText("text1.txt","demo1");
 getText("text2.txt","demo2");
