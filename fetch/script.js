c96d37b5ad08eb279b9645e837930838af276d94
7luas <159937802+7luas@users.noreply.github.com>
2025-02-09 14:18:10 +0000

    d

--- fetch/script.js
+++ fetch/script.js
@@ -1,9 +1,1 @@
-getText("text1.txt","demo1");
-getText("text2.txt","demo2");
-getText("text3.txt","demo3");
-
-async function getText(file,id) {
-  let x = await fetch(file);
-  let y = await x.text();
-  alert(y);
-  document.getElementById(id).innerHTML = y;
+alert("B");
